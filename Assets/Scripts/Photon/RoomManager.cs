318faa9cd2193762e9aa8d31f92dd823a065f2e8
MashaGoro21 <s27179@pjwstk.edu.pl>
2026-07-05 17:46:31 +0200

    RoomNotFound and CannotBeEmpty Messages Added

--- Assets/Scripts/Photon/RoomManager.cs
+++ Assets/Scripts/Photon/RoomManager.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,10 +9,20 @@ public class RoomManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_InputField inputCreate;
     [SerializeField] private TMP_InputField inputJoin;
-    [SerializeField] private TMP_Text emptyRoomNameMessage;
+    [SerializeField] private GameObject messageObject;
+
+    private TMP_Text messageText;
+
+    private void Awake()
+    {
+        messageText = messageObject.GetComponent<TMP_Text>();
+    }
 
     public void CreateRoom()
     {
+        if(!IsRoomNameValid(inputCreate))
+            return;
+
         int selectedLevel = PlayerPrefs.GetInt("Level");
 
         var roomOptions = new RoomOptions
@@ -27,8 +38,19 @@ public class RoomManager : MonoBehaviourPunCallbacks
         PhotonNetwork.CreateRoom(inputCreate.text, roomOptions);
     }
 
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        if(returnCode == ErrorCode.GameIdAlreadyExists)
+        {
+            //
+        }
+    }
+
     public void JoinRoom()
     {
+        if (!IsRoomNameValid(inputJoin))
+            return;
+
         PhotonNetwork.JoinRoom(inputJoin.text);
     }
 
@@ -53,8 +75,42 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     }
 
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        if(returnCode == ErrorCode.GameDoesNotExist)
+        {
+            messageText.text = "Room not found";
+            messageObject.SetActive(true);
+            StartCoroutine(HideMessage());
+        }
+
+        if(returnCode == ErrorCode.GameFull)
+        {
+
+        }
+    }
+
     public void BackButton()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    private bool IsRoomNameValid(TMP_InputField input)
+    {
+        if (string.IsNullOrWhiteSpace(input.text))
+        {
+            messageText.text = "Room name cannot be empty";
+            messageObject.SetActive(true);
+            StartCoroutine(HideMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    private IEnumerator HideMessage()
+    {
+        yield return new WaitForSeconds(2f);
+        messageObject.SetActive(false);
     }
 }
